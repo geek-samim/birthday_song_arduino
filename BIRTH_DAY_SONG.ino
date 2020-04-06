c493fc9fd23d025a6d3cba641328f8c7ca065109
Samim Islam <samimislam9641@gmail.com>
2020-04-06 21:35:41 +0530

    Update BIRTH_DAY_SONG.ino

--- BIRTH_DAY_SONG.ino
+++ BIRTH_DAY_SONG.ino
@@ -6,13 +6,7 @@
  circuit:
  * 8-ohm speaker on digital pin 8
 
- created 21 Jan 2010
- modified 30 Aug 2011
- by Tom Igoe
 
-This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/Tone
 
  */
 #include "pitches.h"
